86c34f80fb1d484cd7d9667e254fccd1a327afe4
Jan <declercqjan@gmail.com>
2020-06-25 17:58:47 +0200

    About to start ch16

---
+++ assets/js/RepLog/RepLogApp.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import RepLogs from "./RepLogs";
+import PropTypes from 'prop-types';
+
+export default class RepLogApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            highlightedRowId: null
+        };
+        this.handleRowClick = this.handleRowClick.bind(this);
+    }
+
+    handleRowClick(repLogId) {
+        this.setState({highlightedRowId: repLogId});
+    }
+
+    render() {
+        const {highlightedRowId} = this.state;
+        const { withHeart } = this.props;
+
+        return <RepLogs
+            highlightedRowId={highlightedRowId}
+            withHeart={withHeart}
+            onRowClick={this.handleRowClick}
+        />
+    }
+}
+
+RepLogApp.propTypes = {
+    withHeart: PropTypes.bool
+}
